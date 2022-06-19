f5bc0e9c1876982a639a7c509f8fad544a410bd2
kangtae1 <kangtae1@naver.com>
2022-06-19 13:52:03 +0900

    modification for 4 drones

--- crazyswarm/ros_ws/src/crazyswarm/scripts/individual_hover.py
+++ crazyswarm/ros_ws/src/crazyswarm/scripts/individual_hover.py
@@ -4,10 +4,6 @@ from __future__ import print_function
 
 from pycrazyswarm import *
 
-
-TAKEOFF_DURATION = 2.5
-HOVER_DURATION = 5.0
-
 def main():
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
@@ -16,9 +12,9 @@ def main():
     for cf in allcfs.crazyflies:
         print(cf.id)
         cf.takeoff(1.0, 2.5)
-        timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
+        timeHelper.sleep(5.0)
         cf.land(0.04, 2.5)
-        timeHelper.sleep(TAKEOFF_DURATION)
+
 
 if __name__ == "__main__":
     main()
